f1ae3a59dd0e6d94af5fae268eef338750385818
Rohan Pareek <rohaniscoding@gmail.com>
2022-04-12 20:31:47 +0530

    updated header ui, added close button to sidebar

--- components/layout/header/index.js
+++ components/layout/header/index.js
@@ -73,36 +73,41 @@ function Header() {
                         <MenuIcon />
                     </button>
                     <Sidebar show={showSidebar} close={closeSidebar}>
-                        <div className={css.logo}>
-                            <Link href="/">
-                                Next Recipe
-                            </Link>
+                        <div className={css['sidebar-container']}>
+                            <div className={css.closeBtn}>
+                                <button onClick={closeSidebar}>&times;</button>
+                            </div>
+                            <div className={css.logo}>
+                                <Link href="/">
+                                    Next Recipe
+                                </Link>
+                            </div>
+                            <nav>
+                                <ul>
+                                    <li>
+                                        <Link href="/">
+                                            <a className={`${asPath === '/' ? css['active-link'] : ''}`}>
+                                                Featured Recipes
+                                            </a>
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/recipes">
+                                            <a className={`${asPath === '/recipes' ? css['active-link'] : ''}`}>
+                                                Browse All Recipes
+                                            </a>
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/recipe/random">
+                                            <a className={`${asPath === '/recipe/random' ? css['active-link'] : ''}`}>
+                                                Random Recipe
+                                            </a>
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </nav>
                         </div>
-                        <nav>
-                            <ul>
-                                <li>
-                                    <Link href="/">
-                                        <a className={`${asPath === '/' ? css['active-link'] : ''}`}>
-                                            Featured Recipes
-                                        </a>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="/recipes">
-                                        <a className={`${asPath === '/recipes' ? css['active-link'] : ''}`}>
-                                            Browse All Recipes
-                                        </a>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="/recipe/random">
-                                        <a className={`${asPath === '/recipe/random' ? css['active-link'] : ''}`}>
-                                            Random Recipe
-                                        </a>
-                                    </Link>
-                                </li>
-                            </ul>
-                        </nav>
                     </Sidebar>
                 </div>
             </div>
